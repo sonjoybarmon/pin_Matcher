6aae1d0e2d3931b57b1e71f7ad53e294f63399f9
Sonjoy Barmon <60999976+sonjoybarmon@users.noreply.github.com>
2020-07-27 21:00:18 +0600

    three time value set

--- js/index.js
+++ js/index.js
@@ -15,8 +15,8 @@ document.getElementById('generateSubmit').addEventListener('click', function(){
     const submitOutputError = document.getElementById('outputError');
         submitOutputError.style.display = 'block';
 
-        var tryCount = document.getElementById("tryCount");
-        var decrease = parseInt(tryCount.innerText)-1;
+        const tryCount = document.getElementById("tryCount");
+        let decrease = parseInt(tryCount.innerText)-1;
         tryCount.innerText = decrease;
         if(tryCount.innerText == 3){
             if(generateInput.value == generateOutput.value){
