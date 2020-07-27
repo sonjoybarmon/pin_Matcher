950597897428c644d76430bc7f3b7342bf554fd4
Sonjoy Barmon <60999976+sonjoybarmon@users.noreply.github.com>
2020-07-27 17:02:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+//Generate Pin 
+document.getElementById('generate_clicker').addEventListener('click',function(){
+    const generateRandomNumber = Math.floor(1000 + Math.random() * 9999);
+    document.getElementById('generate_input').value = generateRandomNumber;
+});
+document.getElementById('generateSubmit').addEventListener('click', function(){
+    const generateInput = document.getElementById('generate_input').value;
+    const generateOutput = document.getElementById('generate_output').value;
+    if(generateInput === generateOutput){
+    const submitOutputDone = document.getElementById('outputDone');
+        submitOutputDone.style.display = 'block';
+    }
+    
+    if(generateInput !== generateOutput){
+    const submitOutputError = document.getElementById('outputError');
+        submitOutputError.style.display = 'block';
+
+        var tryCount = document.getElementById("tryCount");
+        var decrease = parseInt(tryCount.innerText)-1;
+        tryCount.innerText = decrease;
+        if(tryCount.innerText == 3){
+            if(generateInput.value == generateOutput.value){
+                tryCount.innerText= 3 + ' try left';
+            }
+        }
+        if(tryCount.innerText == 2){
+            if(generateInput.value == generateOutput.value){
+                tryCount.innerText= 2 + ' try left';
+            }
+        }
+        if(tryCount.innerText == 1){
+            if(generateInput.value == generateOutput.value){
+                tryCount.innerText= 1 + ' try left';
+            }
+        }
+        if(tryCount.innerText <= 0){
+            if(generateInput.value == generateOutput.value){
+                alert('❌ Please Try Again ')
+                tryCount.innerText= 0 + ' try left';
+            }            
+        }
+    }
+
+});
+
+
+
